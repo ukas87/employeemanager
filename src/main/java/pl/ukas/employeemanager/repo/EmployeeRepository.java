3ef1e604611a73bf139686af462d122fb30c9854
Łukasz Lewandowski <lukastonie@gmail.com>
2022-05-06 12:29:48 +0200

    adding methods in service, creating READEME file

--- src/main/java/pl/ukas/employeemanager/repo/EmployeeRepository.java
+++ src/main/java/pl/ukas/employeemanager/repo/EmployeeRepository.java
@@ -3,5 +3,12 @@ package pl.ukas.employeemanager.repo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import pl.ukas.employeemanager.model.Employee;
 
+import java.util.Optional;
+
 public interface EmployeeRepository extends JpaRepository<Employee,Long> {
+
+    void deleteEmployeeById(Long id);
+
+    Optional findEmployeeById(Long id);
 }
+
